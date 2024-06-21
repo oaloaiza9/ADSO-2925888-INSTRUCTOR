fc1897db56fe1a4e606c12a3826cacced5b2c993
OSCAR LOAIZA <oaloaiza9@misena.edu.co>
2024-06-21 08:16:01 -0500

    Agregando actividad ES y archivo de operadores

---
+++ 03 - Ejemplos/Ejemplo01.java
@@ -0,0 +1,22 @@
+
+public class Ejemplo01 {
+	public static void main(String[] args){
+
+		System.out.println("                         )     ");
+		System.out.println("                       (       ");
+		System.out.println("                      ____     ");
+		System.out.println("                      |  |     ");
+		System.out.println("       /--------------+  +---\\  ");
+		System.out.println("      /                       \\  ");
+		System.out.println("     /-------------------------\\      ()");
+		System.out.println("       |  +===============+  |       (  ) ");
+		System.out.println("       |  |               |  |      (    ) ");
+		System.out.println("       |  +===============+  |     (      )  ");
+		System.out.println("       |---------------------|    (        )    ");
+		System.out.println("       |  +====+   ________  |     (      ) ");
+		System.out.println("       |  |    |   |      |  |       |  |");
+		System.out.println("       |  +====+   |     *|  |       |  |");
+		System.out.println("/\\/\\/\\/|___________|______|__|/\\/\\/\\/|  |/\\/\\/\\");
+
+	}
+}
