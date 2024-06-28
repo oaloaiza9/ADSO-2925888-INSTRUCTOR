42c32b3a3e3dbc49afc4b50df53aee6bfa7160f9
OSCAR LOAIZA <oaloaiza9@misena.edu.co>
2024-06-28 10:11:18 -0500

    Agregando ejercicios de condiciones

---
+++ 03 - Ejemplos/02 - Condiciones/Ejemplo01.java
@@ -0,0 +1,18 @@
+
+public class Ejemplo01{
+	public static void main(String[] args) {
+		
+		int a = 0;
+
+		if ( a > 0 ){
+			System.out.println("El valor de a es POSITIVO.");
+		}else if( a < 0){
+			System.out.println("El valor de a es NEGATIVO.");
+		}else if ( a == 0){
+			System.out.println("El valor de a es CERO.");
+		}
+
+		
+
+	}
+}
