55c286ea761d624c49243b5e58af2f7f4232025b
OSCAR LOAIZA <oaloaiza9@misena.edu.co>
2024-07-08 08:14:08 -0500

    Agregando actividad 02 de condiciones

--- 03 - Ejemplos/02 - Condiciones/Ejemplo01.java
+++ 03 - Ejemplos/02 - Condiciones/Ejemplo01.java
@@ -2,17 +2,17 @@
 public class Ejemplo01{
 	public static void main(String[] args) {
 		
-		int a = 0;
+		int edad = 35;
 
-		if ( a > 0 ){
-			System.out.println("El valor de a es POSITIVO.");
-		}else if( a < 0){
-			System.out.println("El valor de a es NEGATIVO.");
-		}else if ( a == 0){
-			System.out.println("El valor de a es CERO.");
+		if( edad>=0 && edad<=17 ){
+			System.out.println("Menor de Edad - RECIBE AUXILIO DE TRANSPORTE.");
+		}else if( edad>=18 && edad<=59 ){
+			System.out.println("Mayor de Edad - PAGA IMPUESTO");
+		}else if( edad>=60 && edad<=120 ){
+			System.out.println("Tercera Edad - RECIBE AUXILIO DE VEJEZ.");
+		}else if( edad<0 || edad>120){
+			System.out.println("Edad Fuera de Rango.");
 		}
-
-		
 
 	}
 }
